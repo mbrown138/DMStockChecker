0fbc6b8a5b5f7e9e32b2213f9880cc9158c72f9b
Michael <mbrown138@hotmail.com>
2021-01-03 17:27:04 +1100

    Added store search via postcode for ease of use. Removed debugging code. Known issue remains with automatic store selection being based off the town centre.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 
 /*document.cookie ='w-rsjhf=PGcgdD0iY2RiMzJhNmE1ODY1NDFkMzlkZTVmYTZiM2FlNGZlNDdtcndtbWJieXViIiAvPg==; ARRAffinity=329e4c44eb196c5c60ae7386067c7a1d9b57a752c10d3445a00453019855ab9e; ARRAffinitySameSite=329e4c44eb196c5c60ae7386067c7a1d9b57a752c10d3445a00453019855ab9e; bm_sz=2AE4F270D42C968272B852E58E446BEB~YAAQUwUgF7ZneK11AQAAzpaevAn1D//pbmjHgXDYTZSEos4/j0bZjwEeIKflvkWwk9cM4+L72LmjD9tXYjvIe7DZ3BZcr9t55xcVWwIhs+W8oUi7XW+9q2Rajk6D4J6enttWzcdXuc5vRtbGWDzBMyNiMT6rajlZJfenoi79Fwe7ALyiDvO3iP/8uPkmR95I1LrNZZ2mEw==; _abck=BF55C6C3487EFB4774FB43C64BA8E277~-1~YAAQUwUgF7dneK11AQAAzpaevATNkLpxBa+2Ac4gBoaSkqJLMLBjZjhFfG0IfSd4Vn/DMwoAw8k5S69vZ5EVJ+T8TaQLn0oELJKUEiLuiwWNgFVexASgDqc8pD66Wl5bxuHwSggr8htZV57/kFynOZt1KKKU7/AQ6pmFOY7ATYQX/PPo1JzIyxNsH/aBCg/kduHmke0cZdvGXJprDy4szUALfoGXGPeSX5aNBcGCu+se8mML4VepSVFmw1Vc/U4eLL3QKZhpmrxguStYyXNlABjvCSs0q87bJ9eIRW4Wdmn12EWWzGW1NMfUInW8LAHXXg==~-1~-1~-1; bm_sv=B2A2449640F9094F889614C1A514D33B~khxO7C2z6YlCFlsVECAls/ALnp74H1P2UO8dWvB1xjL2IhDQrm6+qoOVZa09JUP3CxMMwWeBr0zsN1vdqPW0WJ/OLVoC/N/hJobWT6J1m9NFkqrQ03E73vgXgottnzAkLcPuyWbXa2vS3bo79DeZ8T3n9O1hRKKH9q3jI0cD9a4=; ak_bmsc=E6951C978FB0305A759753CA368147EE17200553B2460000EF36AD5FEB488017~ploGxzwY93dZIs0nMdSUzmJipmYNQHrVToTqzeqeD7qYR7bKi8PWJIBwYny9j2wNlJu9ER9PqMrKBM35HJB7G4rO+clHD/WH+/pWGC92XJ/SNH5fWmxmULVS8gXq6324mgnPZR3hsHfZ4WKnuRgHFzBP/6Y0t8WZstZIwhENn7UQNSElfM3kbdbIhEUOR0DD74BYg9bk06y47QVf9NLrjPQoAvMHC0wWqypNrATdA8dApinUCKUJx9kywobZJguNO4; w-aqwerhpi=adm-p,i-1568,p-1568,pk-1568,is-dCrSYXpuE58=; w-arjshtsw=xe5c772af6337483886ff5428ae3ff2d2xpunpaele';*/
 
-const stores = require('./stores.json');
+//const stores = require('./stores.json');
 
 const Flexbox = styled.div`
   display:flex;
@@ -44,12 +44,13 @@ function Search(){
   );
 }
 
+//Store Selector
 function StoreSelect(){
   
   return (
     <div>
-      <label htmlFor='store'>Store:</label>
-      <Input id='store'></Input>
+      <label htmlFor='postCode'>Post Code:</label>
+      <Input id='postCode'></Input>
     </div>
   )
 
@@ -57,12 +58,15 @@ function StoreSelect(){
 //Search handler
 async function handleClick(){
   const search = document.getElementById('search').value;
-  const store = document.getElementById('store').value;
+  const code = document.getElementById('postCode').value;
 
   let stockCheck = false;
   if (document.getElementById('stockCheck').checked){
     stockCheck = true;
   }
+
+  const nearbyStores = await storeReq(code);
+  const store = nearbyStores.Stores[0].Id;
 
   const data = await searchReq(search,store);
 
@@ -70,6 +74,16 @@ async function handleClick(){
   ReactDOM.render(
     results, document.getElementById('results')
   );
+}
+
+//Store request
+function storeReq(postCode){
+  const url = `http://localhost:3001/storeGet?postCode=${postCode}`;
+  return fetch(url)
+        .then(response => response.json())
+        .catch(function(e) {
+          console.log(e);
+        });
 }
 
 //Query request
@@ -87,6 +101,7 @@ function Results(props){
   const data = props.data;
   const stockCheck = props.stockCheck;
 
+  //Create array of elements for display
   let products = [];
   if (stockCheck) {
     for (const attr in data.Products) {
@@ -102,7 +117,6 @@ function Results(props){
   }
   
 
-  console.log(data);
   return (
     <Flexbox>
       {products}
@@ -133,6 +147,7 @@ function Product(props){
 
   //const image = `https://media.danmurphys.com.au/dmo/product/${stockcode}-1.png`;
 
+  //Display product cards
   return(
     <Card style={{ width: '20rem' }}>
       <Card.Body>
@@ -149,6 +164,7 @@ function Product(props){
   )
 }
 
+//Main renderer
 ReactDOM.render(
   <React.StrictMode>
     <Search />
@@ -156,9 +172,3 @@ ReactDOM.render(
   document.getElementById('input-field')
 );
 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/server.js
+++ src/server.js
@@ -10,15 +10,22 @@ app.use((req, res, next) => {
   next();
 });
 
+//Product search path
 app.get('/fetch', function (req, res){
-  gethtml(req, res);  
+  getProduct(req, res);  
+
+})
+
+//Store search path
+app.get('/storeGet', function (req, res){
+  getStore(req, res);  
 
 })
 
 app.listen(3001)
 
-//Get and send requested page
-async function gethtml(req, res){
+//Get and send product query response
+async function getProduct(req, res){
   const url = 'https://api.danmurphys.com.au/apis/ui/Search/products';
 
   let searchParams = {
@@ -35,13 +42,12 @@ async function gethtml(req, res){
   const store = req.query.store;
   const cookie = `w-aqwerhpi=adm-p,i-${store},p-${store},pk-${store},is-dCrSYXpuE58=;`;
 
-  console.log(search);
-
-  const data = await getData(url, search, cookie);
+  const data = await postData(url, search, cookie);
   res.send(data.data);
 }
 
-function getData(url, search, cookie){
+//Posts product query
+function postData(url, search, cookie){
   return axios({
     url: url,
     method: 'post',
@@ -60,3 +66,29 @@ function getData(url, search, cookie){
       console.log(e);
     });
   }
+
+//Gets store data by postcode 
+async function getStore(req, res){
+  const postCode = req.query.postCode;
+  let url = `https://api.danmurphys.com.au/apis/ui/StoreLocator/Stores/danmurphys?PostCode=${postCode}`;
+
+  const data = await getData(url);
+  res.send(data.data);
+}
+
+//Gets store data
+function getData(url, cookie) {
+  return axios({
+    url: url,
+    method: 'get',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+
+      'Content-Type': 'application/json',
+    }
+  }
+    )  
+    .catch(function(e) {
+      console.log(e);
+    });
+}
