12a63e61f3cb4dc797f8fa77dd65a40e0d3f2fd2
Michael <mbrown138@hotmail.com>
2021-01-07 17:21:25 +1100

    Added current store name. Cleaned up auto-generated code and files. Formalised client-side comments. Server-side comments unchanged pending deployment.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
-/*document.cookie ='w-rsjhf=PGcgdD0iY2RiMzJhNmE1ODY1NDFkMzlkZTVmYTZiM2FlNGZlNDdtcndtbWJieXViIiAvPg==; ARRAffinity=329e4c44eb196c5c60ae7386067c7a1d9b57a752c10d3445a00453019855ab9e; ARRAffinitySameSite=329e4c44eb196c5c60ae7386067c7a1d9b57a752c10d3445a00453019855ab9e; bm_sz=2AE4F270D42C968272B852E58E446BEB~YAAQUwUgF7ZneK11AQAAzpaevAn1D//pbmjHgXDYTZSEos4/j0bZjwEeIKflvkWwk9cM4+L72LmjD9tXYjvIe7DZ3BZcr9t55xcVWwIhs+W8oUi7XW+9q2Rajk6D4J6enttWzcdXuc5vRtbGWDzBMyNiMT6rajlZJfenoi79Fwe7ALyiDvO3iP/8uPkmR95I1LrNZZ2mEw==; _abck=BF55C6C3487EFB4774FB43C64BA8E277~-1~YAAQUwUgF7dneK11AQAAzpaevATNkLpxBa+2Ac4gBoaSkqJLMLBjZjhFfG0IfSd4Vn/DMwoAw8k5S69vZ5EVJ+T8TaQLn0oELJKUEiLuiwWNgFVexASgDqc8pD66Wl5bxuHwSggr8htZV57/kFynOZt1KKKU7/AQ6pmFOY7ATYQX/PPo1JzIyxNsH/aBCg/kduHmke0cZdvGXJprDy4szUALfoGXGPeSX5aNBcGCu+se8mML4VepSVFmw1Vc/U4eLL3QKZhpmrxguStYyXNlABjvCSs0q87bJ9eIRW4Wdmn12EWWzGW1NMfUInW8LAHXXg==~-1~-1~-1; bm_sv=B2A2449640F9094F889614C1A514D33B~khxO7C2z6YlCFlsVECAls/ALnp74H1P2UO8dWvB1xjL2IhDQrm6+qoOVZa09JUP3CxMMwWeBr0zsN1vdqPW0WJ/OLVoC/N/hJobWT6J1m9NFkqrQ03E73vgXgottnzAkLcPuyWbXa2vS3bo79DeZ8T3n9O1hRKKH9q3jI0cD9a4=; ak_bmsc=E6951C978FB0305A759753CA368147EE17200553B2460000EF36AD5FEB488017~ploGxzwY93dZIs0nMdSUzmJipmYNQHrVToTqzeqeD7qYR7bKi8PWJIBwYny9j2wNlJu9ER9PqMrKBM35HJB7G4rO+clHD/WH+/pWGC92XJ/SNH5fWmxmULVS8gXq6324mgnPZR3hsHfZ4WKnuRgHFzBP/6Y0t8WZstZIwhENn7UQNSElfM3kbdbIhEUOR0DD74BYg9bk06y47QVf9NLrjPQoAvMHC0wWqypNrATdA8dApinUCKUJx9kywobZJguNO4; w-aqwerhpi=adm-p,i-1568,p-1568,pk-1568,is-dCrSYXpuE58=; w-arjshtsw=xe5c772af6337483886ff5428ae3ff2d2xpunpaele';*/
 
-//const stores = require('./stores.json');
-
+//Declaring React-Bootstrap elements
 const Flexbox = styled.div`
   display:flex;
   flex-wrap:wrap;
@@ -27,7 +22,10 @@ const Input = styled.input`
 const StyledButton = styled(Button)`
   margin:1em;`
   
-//Search field
+/**
+  * @desc contains layout and functionality for user-input
+  * @return jsx for user-input fields
+*/
 function Search(){
   return(
     
@@ -44,7 +42,10 @@ function Search(){
   );
 }
 
-//Store Selector
+/**
+  * @desc post-code entry component
+  * @return jsx for postcode entry
+*/
 function StoreSelect(){
   
   return (
@@ -55,7 +56,10 @@ function StoreSelect(){
   )
 
 }
-//Search handler
+
+/**
+  * @desc handler for search function, renders Results component
+*/
 async function handleClick(){
   const search = document.getElementById('search').value;
   const code = document.getElementById('postCode').value;
@@ -67,16 +71,22 @@ async function handleClick(){
 
   const nearbyStores = await storeReq(code);
   const store = nearbyStores.Stores[0].Id;
+  const storeName = nearbyStores.Stores[0].Name;
 
   const data = await searchReq(search,store);
 
-  const results = <Results data={data} stockCheck={stockCheck}/>
+  const results = <Results data={data} stockCheck={stockCheck} storeName={storeName}/>
   ReactDOM.render(
     results, document.getElementById('results')
   );
 }
 
-//Store request
+
+/**
+  * @desc requests nearby store data from server
+  * @param postCode post-code entered by user in field
+  * @return json object with nearby store data
+*/
 function storeReq(postCode){
   const url = `http://localhost:3001/storeGet?postCode=${postCode}`;
   return fetch(url)
@@ -86,7 +96,12 @@ function storeReq(postCode){
         });
 }
 
-//Query request
+/**
+  * @desc requests inventory data from server
+  * @param search product name to be queried
+  * @param store number of store to be queried
+  * @return json object with product data
+*/
 function searchReq(search,store){
   const url = `http://localhost:3001/fetch?search=${search}&store=${store}`;
   return fetch(url)
@@ -96,10 +111,17 @@ function searchReq(search,store){
         });
 }
 
-//Results display
+/**
+  * @desc Results component, packs relevant objects into array from raw json
+  * @param props.data product json object
+  * @param props.stockCheck OOS tickbox value
+  * @param props.storeName name of store
+  * @return jsx of store name and results
+*/
 function Results(props){
   const data = props.data;
   const stockCheck = props.stockCheck;
+  const storeName = props.storeName;
 
   //Create array of elements for display
   let products = [];
@@ -118,14 +140,21 @@ function Results(props){
   
 
   return (
-    <Flexbox>
-      {products}
-    </Flexbox>
+    <div>
+      <h2>{storeName}</h2>
+      <Flexbox>
+        {products}
+      </Flexbox>
+    </div>
   )
  
 }
 
-//Creates individual product results
+/**
+  * @desc creates individual product cards for display
+  * @param item object with data for individual product
+  * @return jsx for product card
+*/
 function Product(props){
   let name = props.item.Name;
   name = name.replace('<br>', ' ');
@@ -134,7 +163,6 @@ function Product(props){
   const singlePrice = props.item.Products[0].Prices.singleprice;
   let promoPrice;
   let casePrice;
-  //let limitedStock;
 
   if (props.item.Products[0].Prices.hasOwnProperty('promoprice')){
     promoPrice = <ListGroupItem>Promo: {props.item.Products[0].Prices.promoprice.Value} {props.item.Products[0].Prices.promoprice.Message}</ListGroupItem>
@@ -143,11 +171,6 @@ function Product(props){
     casePrice = <ListGroupItem>Case: {props.item.Products[0].Prices.caseprice.Value} {props.item.Products[0].Prices.caseprice.Message}</ListGroupItem>
   }
 
-  //if (props.item.Products[0]){}
-
-  //const image = `https://media.danmurphys.com.au/dmo/product/${stockcode}-1.png`;
-
-  //Display product cards
   return(
     <Card style={{ width: '20rem' }}>
       <Card.Body>
